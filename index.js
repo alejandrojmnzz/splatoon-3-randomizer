907c271307e6f10ee1efe703f546a82c6a534333
Alejandro <alejandro12850mj@gmail.com>
2025-05-11 19:49:18 -0400

    deploy

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
+
 let weapons = []
 let abilities = []
 let exclusiveAbilities = false
+
 
 window.onload = () => {
     axios.get('http://localhost:3000/weapons')
